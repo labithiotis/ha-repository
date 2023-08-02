f0d84a9d34b7fee8781a640341e286f951c5969e
Darren Labithiotis <labithiotis@gmail.com>
2023-08-02 07:30:18 +0100

    try addding jq in script

--- n8n/docker-entrypoint.sh
+++ n8n/docker-entrypoint.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bashio
+#!/usr/bin/sh
+
+if ! [ -x "$(command -v jq)" ]; then
+  apk update && apk upgrade && apk add --no-cache jq
+fi
 
 # Loop through all keys in options.json and export them
 for s in $(cat options.json | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
