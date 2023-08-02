84f08836ddd429f3b70a8b384bbcf4a5fa35e6a9
labithiotis <labithiotis@gmail.com>
2023-08-02 09:23:09 +0200

    chore: switch user to install jq

--- n8n/docker-entrypoint.sh
+++ n8n/docker-entrypoint.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/sh
-
-if ! [ -x "$(command -v jq)" ]; then
-  apk update && apk upgrade && apk add --no-cache jq
-fi
+#!/usr/bin/with-contenv bashio
 
 # Loop through all keys in options.json and export them
 for s in $(cat options.json | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
